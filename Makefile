blinkhealth/go-config-yourself

commit e8af22030628276782ad6d2539603488a2742e0c

feature: Build for ARMv7 (#18)
build dependencies and binary for arm7

Makefile
@@ -72,7 +72,7 @@ build: dist build-deps dist/gcy-macos-amd64.tgz dist/gcy-linux-amd64.tgz debian
 build-xgo: dist
 	# produce debug-symbol stripped binaries
 	$(GOBIN)/xgo --image xgo \
-		--targets 'linux/amd64 darwin-10.10/amd64' \
+		--targets 'linux/amd64 linux/arm-7 darwin-10.10/amd64'
 		--out 'dist/gcy' \
 		--ldflags "-s -w -X main.version=$(shell sed 's/^v//' dist/VERSION)" \
 		$(ROOT_DIR)
@@ -83,7 +83,7 @@ compress-binaries:
 	docker run --rm --tty \
 		-v $(ROOT_DIR)/dist:/target \
 		--entrypoint upx \
-		xgo -9 --no-progress /target/gcy-linux-amd64 /target/gcy-macos-amd64
+		xgo -9 --no-progress /target/gcy-linux-amd64 /target/gcy-macos-amd64 /target/gcy-linux-arm-7
 
 build-local: dist
 	mkdir -p dist/local
@@ -102,13 +102,23 @@ dist/gcy-macos-amd64.tgz: docs build-xgo
 
 dist/gcy-linux-amd64.tgz: docs build-xgo
 	mkdir -p dist/linux
-	cp dist/gcy-linux-amd64 dist/linux/gcy
+	cp dist/gcy-linux-arm-7 dist/linux/gcy
 	cp -r bin/autocomplete dist/linux
 	cp -r dist/docs/man dist/linux
 	cp bin/build/linux.Makefile dist/linux/Makefile
 	tar -czf dist/gcy-linux-amd64.tgz -C "$(ROOT_DIR)/dist/linux" .
 	openssl dgst -sha256 dist/gcy-linux-amd64.tgz | awk '{print $$2}' > dist/gcy-linux-amd64.shasum
 	rm -rf dist/linux
+
+dist/gcy-linux-arm.tgz: docs build-xgo
+	mkdir -p dist/arm
+	cp dist/gcy-linux-arm dist/arm/gcy
+	cp -r bin/autocomplete dist/arm
+	cp -r dist/docs/man dist/arm
+	cp bin/build/linux.Makefile dist/arm/Makefile
+	tar -czf dist/gcy-linux-arm.tgz -C "$(ROOT_DIR)/dist/linux" .
+	openssl dgst -sha256 dist/gcy-linux-arm.tgz | awk '{print $$2}' > dist/gcy-linux-arm.shasum
+	rm -rf dist/arm
 
 build-deps:
 	cd / && GO111MODULE=auto go get -u src.techknowlogick.com/xgo

bin/build/gpgme.sh
@@ -4,7 +4,7 @@
 set -o errexit
 
 BUILD_ROOT="/build-libs"
-targets=(linux darwin)
+targets=(linux darwin arm)
 gpgme="$BUILD_ROOT/gpgme"
 libs=("$BUILD_ROOT/libgpg-error" "$BUILD_ROOT/libassuan")
 for target in "${targets[@]}"; do
@@ -19,26 +19,43 @@ done
 # Quiet down
 export OSXCROSS_NO_INCLUDE_PATH_WARNINGS=1
 buildEnv() {
-  if [[ $1 == "darwin" ]]; then
-    HOST=x86_64-apple-darwin
-    # GPGME requires host to be different, but HOST to be set
-    # I don't quite understand why, but there's only that directory in the source
-    export GPGME_HOST='x86_64-apple-darwin15'
-    # use special compilers
-    export CC=o64-clang
-    export CXX=o64-clang++
-    # use special libtool and friends
-    export LIBTOOL="x86_64-apple-darwin15-libtool"
-    export AR="x86_64-apple-darwin15-ar"
-    export RANLIB="x86_64-apple-darwin15-ranlib"
-    # This is where the compiled targets go
-    export PREFIX=/usr/local/osx-ndk-x86/SDK/MacOSX10.11.sdk/usr
-    export PATH="/usr/local/osx-ndk-x86/bin:$PATH"
-  else
-    HOST=x86_64-linux
-    export GPGME_HOST="$HOST"
-    export PREFIX=/usr/local
-  fi
+  case $1 in
+    darwin)
+      HOST=x86_64-apple-darwin
+      # GPGME requires host to be different, but HOST to be set
+      # I don't quite understand why, but there's only that directory in the source
+      export GPGME_HOST='x86_64-apple-darwin15'
+      # use special compilers
+      export CC=o64-clang
+      export CXX=o64-clang++
+      # use special libtool and friends
+      export LIBTOOL="x86_64-apple-darwin15-libtool"
+      export AR="x86_64-apple-darwin15-ar"
+      export RANLIB="x86_64-apple-darwin15-ranlib"
+      # This is where the compiled targets go
+      export PREFIX=/usr/local/osx-ndk-x86/SDK/MacOSX10.11.sdk/usr
+      export PATH="/usr/local/osx-ndk-x86/bin:$PATH"
+      ;;
+    linux)
+      HOST=x86_64-linux
+      export GPGME_HOST="$HOST"
+      export PREFIX=/usr/local
+      ;;
+    arm)
+      HOST=arm-linux-gnueabihf
+      export GPGME_HOST="$HOST"
+      export CC=arm-linux-gnueabihf-gcc-5
+      export CXX=arm-linux-gnueabihf-g++-5
+      export PREFIX=/usr/arm-linux-gnueabihf
+      export CFLAGS="-march=armv7-a -fPIC"
+      export CXXFLAGS="-march=armv7-a -fPIC"
+      export PKG_CONFIG_PATH=/usr/arm-linux-gnueabihf/lib/pkgconfig
+      export AR="arm-linux-gnueabihf-ar"
+      export LD="arm-linux-gnueabihf-ld"
+      export CPP="arm-linux-gnueabihf-cpp-5"
+      export STRIP="arm-linux-gnueabihf-strip"
+      export RANLIB="arm-linux-gnueabihf-ranlib"
+  esac
   export HOST
 }
 

commit d264e825233556ba7eea1b088db8cf22ffc4734c

Fix broken builds, build with golang 0.12.x
Turns out I broke the builds with #18 due to a missing backslash. I learned a bit more about `CFLAGS` and `LDFLAGS` which let us specify the paths to built libraries and included headers, I suspect by default it corresponds to [`/usr(/local)?`](https://en.wikipedia.org/wiki/Unix_filesystem#Conventional_directory_layout). I set these to the corresponding path for [osxcross](https://github.com/tpoechtrager/osxcross) (the cross-compilation toolchain for MacOS that is used by [xgo](https://github.com/tpoechtrager/osxcross)), `/usr/local/osx-ndk-x86/SDK/MacOSX10.11.sdk/usr`; these static libraries and headers are dropped there after building in `bin/build/gpgme.sh`. Same thing happens with ARM, however, it seems the toolchain for linux handles this automagically.

- Build with golang 0.12x

Makefile
@@ -72,7 +72,7 @@ build: dist build-deps dist/gcy-macos-amd64.tgz dist/gcy-linux-amd64.tgz debian
 build-xgo: dist
 	# produce debug-symbol stripped binaries
 	$(GOBIN)/xgo --image xgo \
-		--targets 'linux/amd64 linux/arm-7 darwin-10.10/amd64'
+		--targets 'linux/amd64 linux/arm-7 darwin-10.10/amd64' \
 		--out 'dist/gcy' \
 		--ldflags "-s -w -X main.version=$(shell sed 's/^v//' dist/VERSION)" \
 		$(ROOT_DIR)

bin/build/Dockerfile
@@ -1,4 +1,5 @@
-FROM techknowlogick/xgo:go-1.11.8
+FROM techknowlogick/xgo:go-1.12.x
+# see https://github.com/techknowlogick/xgo
 # Used by go-config-yourself to cross compile to MacOS and Linux from CircleCI
 
 ENV GPGME_VERSION "1.13.0"
@@ -29,3 +30,5 @@ COPY gpgme.sh /build-libs
 RUN /build-libs/gpgme.sh
 # clean up
 RUN rm -rf /build-libs
+# specify correct prefix for osx cgo builds
+RUN sed -i 's|GOOS=darwin GOARCH=amd64|GOOS=darwin GOARCH=amd64 CGO_CFLAGS="-I/usr/local/osx-ndk-x86/SDK/MacOSX10.11.sdk/usr/include" CGO_LDFLAGS="-L/usr/local/osx-ndk-x86/SDK/MacOSX10.11.sdk/usr/lib"|' /build.sh

bin/build/gpgme.sh
@@ -4,7 +4,7 @@
 set -o errexit
 
 BUILD_ROOT="/build-libs"
-targets=(linux darwin arm)
+targets=(linux arm darwin)
 gpgme="$BUILD_ROOT/gpgme"
 libs=("$BUILD_ROOT/libgpg-error" "$BUILD_ROOT/libassuan")
 for target in "${targets[@]}"; do
@@ -55,6 +55,9 @@ buildEnv() {
       export CPP="arm-linux-gnueabihf-cpp-5"
       export STRIP="arm-linux-gnueabihf-strip"
       export RANLIB="arm-linux-gnueabihf-ranlib"
+      ;;
+    default)
+      >&2 echo "Unknown target $1"
   esac
   export HOST
 }
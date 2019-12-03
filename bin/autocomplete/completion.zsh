948eff7410751de44b5c334fb33116008315db38
Roberto Hidalgo <rob@blinkhealth.com>
2019-12-02 22:31:08 -0500

    fix zsh completion script

--- bin/autocomplete/completion.zsh
+++ bin/autocomplete/completion.zsh
@@ -3,19 +3,17 @@
 # shellcheck shell=bash
 
 _gcy_zsh_autocomplete () {
-
   local -a opts
-  opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 ${words[@]:0:#words[@]-1} --generate-bash-completion)}")
+  cur="${words[-1]}"
+  opts=("${(@f)$(_CLI_ZSH_AUTOCOMPLETE_HACK=1 CUR="$cur" ${words[@]:0:#words[@]-1} --generate-bash-completion)}")
 
   exit_code="$?"
   if [[ $exit_code -gt 0 ]]; then
     _path_files
-    [[ $exit_code == 1 ]]; return
+    return
   fi
 
-  _describe 'gcy' opts
-
-  return
+  _describe "qwer" opts
 }
 
 compdef _gcy_zsh_autocomplete gcy
